9ba71841824d6178155debae1a9fbe9f259fe31d
eveciancia <cianci96@students.rowan.edu>
2016-02-01 16:27:23 -0500

    added a note!

--- app/src/main/java/edu/rowan_senior_project/senior_project/MainActivity.java
+++ app/src/main/java/edu/rowan_senior_project/senior_project/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 // test change 222 by John and Ben
-
+// test change by eve!
 public class MainActivity extends AppCompatActivity {
 
     @Override
