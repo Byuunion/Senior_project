35e1192545c6b5d927d27dcbcb33dedc1ce7004a
Benson Xu <ormegabenson71593@msn.com>
2016-02-01 15:11:58 -0500

    Initial commit

---
+++ app/src/main/java/edu/rowan_senior_project/senior_project/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.rowan_senior_project.senior_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
