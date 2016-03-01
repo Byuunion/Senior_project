ab85ede1ac5ff2131d21415d13b45d51222c707a
Choyce60 <Choyce60@students.rowan.edu>
2016-03-01 00:56:03 -0500

    Simple Login Page (No functionality yet)

--- Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable.Droid/Resources/Resource.designer.cs
+++ Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable.Droid/Resources/Resource.designer.cs
@@ -45,7 +45,16 @@ namespace MeetMeet_Native_Portable.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int ButtonSignInStyle = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int ButtonSignUpStyle = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int ProgressBarStyle = 2130837507;
 			
 			static Drawable()
 			{
@@ -61,7 +70,19 @@ namespace MeetMeet_Native_Portable.Droid
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int MeetMeet = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int SignIn = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int SignUp = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int progressBar1 = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int txtOr = 2131034114;
 			
 			static Id()
 			{
