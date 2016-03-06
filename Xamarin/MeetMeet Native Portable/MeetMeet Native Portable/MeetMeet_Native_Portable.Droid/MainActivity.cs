1e1944be691121fef1b78f66deaec77f9af295ce
chouze <cehouze@gmail.com>
2016-03-06 16:31:39 -0500

    Added api communication functionality
    Added several packages to allow us to communicate with the server.
    Created a working post request to post user information into the
    database.

--- Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable.Droid/MainActivity.cs
+++ Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable.Droid/MainActivity.cs
@@ -6,21 +6,93 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MeetMeet_Native_Portable.Droid
 {
-	[Activity (Label = "MeetMeet_Native_Portable.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
+    [Activity(Label = "MeetMeet_Native_Portable.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Person test = new Person();
+            System.Diagnostics.Debug.WriteLine("hello");
+            PostUser("", test);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
-		}
-	}
+        }
+
+        
+
+        private async Task PostUser(string url, Person person)
+        {
+            System.Diagnostics.Debug.WriteLine("Username " + person.username);
+            HttpClient client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+
+            string RestURL = "http://52.91.212.179:8800/user/";
+
+            var uri = new Uri(string.Format(RestURL, person.username));
+
+            var json = JsonConvert.SerializeObject(person);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = null;
+
+            System.Diagnostics.Debug.WriteLine("Waiting for response");
+
+            //response = client.PostAsync(uri, content);
+
+            var task = client.PostAsync(uri, content);
+
+            if (await Task.WhenAny(task, Task.Delay(10000)) == task)
+            {
+                System.Diagnostics.Debug.WriteLine("Response received");
+                System.Diagnostics.Debug.WriteLine(task.Status);
+
+            }
+            else {
+                System.Diagnostics.Debug.WriteLine("Response timeout");
+            }
+
+
+            System.Diagnostics.Debug.WriteLine("Done waiting for response");
+
+            if (response.IsSuccessStatusCode)
+            {
+                System.Diagnostics.Debug.WriteLine(@"             TodoItem successfully saved.");
+
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Not Successful");
+            }
+
+        }
+
+    }
+
+    public class Person
+    {
+        public string username = "notChris";
+        public string first_name = "Christopher";
+        public string last_name = "Houze";
+        public int positive_votes = 1;
+        public int negative_votes = 1;
+        public int current_lat = 12;
+        public int current_long = 21;
+        public string gender = "Male";
+        public string bio = "This is a bio";
+
+    }
 }
 
 
