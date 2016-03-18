bd53dc7ec6accaf9d1fdd5aa60e475c12b69a205
chouze <cehouze@gmail.com>
2016-03-18 15:54:41 -0400

    Added Gettable class
    For some reason the gettable class didn't sync with the last commit

---
+++ Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable/Getter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace MeetMeet_Native_Portable
+{
+    public class Getter<T> where T : Gettable
+    {
+
+        public static async Task<T> GetObject(T obj, string url)
+        {
+            HttpClient client;
+            T returnData = default(T);
+            var uri = new Uri(string.Format(url, obj.GetGetName()));
+
+            client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+
+            var response = await client.GetAsync(uri);
+
+            if (response.IsSuccessStatusCode)
+            {
+                System.Diagnostics.Debug.WriteLine("Response was not successful");
+                var content = await response.Content.ReadAsStringAsync();
+                returnData = JsonConvert.DeserializeObject<List<T>>(content).ElementAt(0);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Response was not successful");
+            }
+
+            return returnData;
+        }
+
+
+    }
+}
