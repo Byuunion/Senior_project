3937d76f0162a786b09f7b67c3fc42dc03e8d25c
Arkhon919 <wyngs919@gmail.com>
2016-03-28 17:28:08 -0400

    Profile Skeleton
    Added base functionality for the Profile User Interface and Activities

---
+++ Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable.Droid/ProfileActivity.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MeetMeet_Native_Portable.Droid
+{
+	public class OnProfileEventArgs: EventArgs {
+
+		public string mUsername;
+		public int mPositive_votes;
+		public int mNegative_votes;
+		public double mCurrent_lat;
+		public double mCurrent_long;
+		public string mGender;
+		public string mBio;
+
+
+		public string Username{
+			get { return mUsername; }
+			set { mUsername = value; }
+		}
+
+		public int Positive_Votes{
+			get { return mPositive_votes; }
+			set { mPositive_votes = value; }
+		}
+
+		public int Negative_Votes{
+			get { return mNegative_votes; }
+			set { mNegative_votes = value; }
+		}
+
+		public double Current_lat{
+			get { return mCurrent_lat; }
+			set { mCurrent_lat = value; }
+		}
+
+		public double Current_long{
+			get { return mCurrent_long; }
+			set { mCurrent_long = value; }
+		}
+
+		public string Gender{
+			get { return mGender; }
+			set { mGender = value; }
+		}
+
+		public string Bio {
+			get { return mBio; }
+			set { mBio = value; }
+		}
+
+	}
+
+
+	[Activity (Label = "Profile")]
+	public class Profile : DialogFragment{
+
+		private EditText mTxtUsername;
+		private EditText mTxtGender;
+		private EditText mTxtBio;
+
+
+	}
+
+}
--- Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable/Profile.cs
+++ Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable/Profile.cs
@@ -2,34 +2,111 @@
 
 namespace MeetMeet_Native_Portable 
 {
-    public class Profile : Updatable
-    {
-        public string username;
-        public int positive_votes;
-        public int negative_votes;
-        public double current_lat;
-        public double current_long;
-        public string gender;
-        public string bio;
-        public string token;
+	public class Profile : Postable
+	{
+		public string username;
+		public string first_name;
+		public string last_name;
+		public int positive_votes;
+		public int negative_votes;
+		public double current_lat;
+		public double current_long;
+		public int age;
+		public string gender;
+		public string bio;
 
 
+		public Profile(string username, string first_name, string last_name, string gender, int age){
+			this.username = username;
+			this.first_name = first_name;
+			this.last_name = last_name;
+			this.gender = gender;
+			this.age = age;
+			this.positive_votes = 0;
+			this.negative_votes = 0;
+			this.current_lat = 0;
+			this.current_long = 0;
+			this.bio = "User has not set up a bio yet.";
+		}
 
-        public Profile(string username, string gender, string bio, string token)
-        {
-            this.username = username;
-            this.gender = gender;
-            this.positive_votes = 0;
-            this.negative_votes = 0;
-            this.current_lat = 0.0;
-            this.current_long = 0.0;
-            this.bio = bio;
-            this.token = token;
-        }
+		public string getName(){
+			return this.username;
+		}
 
-        public string GetName()
-        {
-            return this.username;
-        }
-    }
+		public void setName(string username){
+			this.username = username;
+		}
+
+		public string getFirstName(){
+			return this.first_name;
+		}
+
+		public void setFirstName(string first_name){
+			this.first_name = first_name;
+		}
+
+		public string getLastName(){
+			return this.last_name;
+		}
+
+		public void setLastName(string last_name){
+			this.last_name = last_name;
+		}
+
+		public string getGender(){
+			return this.gender;
+		}
+
+		public void setGender(string gender){
+			this.gender = gender;
+		}
+
+		public int getAge(){
+			return age;
+		}
+
+		public void setAge(int age){
+			this.age = age;
+		}
+
+		public string getBio(){
+			return this.bio;
+		}
+
+		public void setBio(string bio){
+			this.bio = bio;
+		}
+
+		public int getPosVotes(){
+			return this.positive_votes;
+		}
+
+		public void setPosVotes(int positive_votes){
+			this.positive_votes = positive_votes;
+		}
+
+		public int getNegVotes(){
+			return this.negative_votes;
+		}
+
+		public void setNegVotes(int negative_votes){
+			this.negative_votes = negative_votes;
+		}
+
+		public double getCurrLong(){
+			return this.current_long;
+		}
+
+		public void setCurrLong(double current_long){
+			this.current_long = current_long;
+		}
+
+		public double getCurrLat(){
+			return this.current_lat;
+		}
+
+		public void setCurrLat(double current_lat){
+			this.current_lat = current_lat;
+		}
+	}
 }
