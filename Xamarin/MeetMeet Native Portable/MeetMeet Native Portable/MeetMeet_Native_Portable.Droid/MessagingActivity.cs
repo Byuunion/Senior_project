2d39c0c949b57b26323d262557742192a501cb61
eveciancia <cianci96@students.rowan.edu>
2016-04-11 18:37:06 -0400

    Updated Send Msg as well as Msg Activity
    Converted send msg into a pop up dialogue that way when we implement
    the button on a user’s profile it won’t open up another layout view.

--- Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable.Droid/MessagingActivity.cs
+++ Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable.Droid/MessagingActivity.cs
@@ -14,13 +14,38 @@ using Android.Widget;
 namespace MeetMeet_Native_Portable.Droid
 {
 	[Activity (Label = "MessagingActivity")]			
-	public class MessagingActivity : Activity
+	public class MessagingActivity : DialogFragment
 	{
-		protected override void OnCreate (Bundle savedInstanceState)
-		{
-			base.OnCreate (savedInstanceState);
 
+		private Button mBtnSendMsg;
+		private TextView mUsernameTextView;
+		private TextView mMsgTextTextView;
+
+		public static Credentials credentials;
+		public static string serverURL = "http://52.91.212.179:8800/";
+
+		public string userNameReciever;
+
+		public override View OnCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState)
+		{
+			base.OnCreateView (inflator, container, savedInstanceState);
+			var view = inflator.Inflate (Resource.Layout.dialog_send_msg, container, false);
+			//SetContentView (Resource.Layout.dialog_send_msg);
+
+			mUsernameTextView = view.FindViewById<EditText> (Resource.Id.editsenduser);
+			mMsgTextTextView =	view.FindViewById<EditText> (Resource.Id.edittextmsg);
+			mBtnSendMsg = view.FindViewById<Button> (Resource.Id.btnSendMsg); 
 			// Create your application here
+
+			mBtnSendMsg.Click += mBtnSendMsg_Click;
+				
+
+			return view;
+		}
+
+		void mBtnSendMsg_Click(object sender, EventArgs e)
+		{
+			MessageSender.SendSingleMessage(mUsernameTextView.Text, mMsgTextTextView.Text , MainActivity.credentials, "http://52.91.212.179:8800/user/message");
 		}
 	}
 }
--- Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable/MessageSender.cs
+++ Xamarin/MeetMeet Native Portable/MeetMeet Native Portable/MeetMeet_Native_Portable/MessageSender.cs
@@ -9,25 +9,25 @@ namespace MeetMeet_Native_Portable
     public class MessageSender
     {
         /// <param name="url"></param> http://52.91.212.179:8800/user/message
-        public async Task<Boolean> SendSingleMessage(String message, string username_to, Credentials this_user, string url)
+        public static async Task<Boolean> SendSingleMessage(String message, string username_to, Credentials this_user, string url)
         {
             return await Poster.PostObject(new { username_to = username_to, username_from = this_user.username, token = this_user.token, message_code = "1", message_text = message}, url);
         }
 
         /// <param name="url"></param> http://52.91.212.179:8800/user/group/message
-        public async Task<Boolean> SendGroupMessage(String message, Credentials this_user, string url)
+        public static async Task<Boolean> SendGroupMessage(String message, Credentials this_user, string url)
         {
             return await Poster.PostObject(new { username_from = this_user.username, token = this_user.token, message_code = "2", message_text = message}, url);
         }
 
         /// <param name="url"></param> http://52.91.212.179:8800/user/message
-        public async Task<Boolean> SendUtilSingleMessage(string username_to, Credentials this_user, string url, string message_code)
+        public static async Task<Boolean> SendUtilSingleMessage(string username_to, Credentials this_user, string url, string message_code)
         {
             return await Poster.PostObject(new { username_inviter = username_to, username_responder = this_user.username, token = this_user.token, message_code = message_code}, url);
         }
 
         /// <param name="url"></param> http://52.91.212.179:8800/user/message/user/group/invite
-        public async Task<Boolean> SendGroupInvite(string username_to, Credentials this_user, string url)
+        public static async Task<Boolean> SendGroupInvite(string username_to, Credentials this_user, string url)
         {
             return await Poster.PostObject(new { username_inviter = username_to, username_responder = this_user.username, token = this_user.token }, url);
         }
